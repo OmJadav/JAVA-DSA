856ee50ffc6e0d988e14fb39d3178e0868bea822
Om Jadav <omjadav04@gmail.com>
2024-02-06 16:58:29 +0530

    hello world

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello{
+    public static void main(String args[]){
+        printHello();
+    }
+
+
+    public static void printHello(){
+        System.out.println("hello world in function");
+    }
+}
