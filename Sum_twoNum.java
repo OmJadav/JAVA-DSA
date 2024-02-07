378cf24df1e7e53139cb2460d4a99aedae0986e4
Om Jadav <omjadav04@gmail.com>
2024-02-07 22:10:21 +0530

    sum of two numbers

---
+++ Sum_twoNum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Sum_twoNum {
+    public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+sumTwoNum(a, b);
+    }
+
+    public static void sumTwoNum(int a,int b){
+        int sum=a+b;
+        System.out.println("Total Sum is::"+sum);
+    }
+}
