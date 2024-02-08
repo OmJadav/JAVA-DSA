573d4e73d499cfaf325af71b308b6dde84a460ec
Om Jadav <omjadav04@gmail.com>
2024-02-08 18:13:21 +0530

    Prime.java

--- Prime.java
+++ Prime.java
@@ -1,12 +1,13 @@
 public class Prime {
     public static void main(String[] args) {
-        System.out.println(isPrime(4));
+        System.out.println(isPrime(2));
     }
 
     public static String isPrime(int n){
-        if(n==1){
-            return "Prime Number";
+        if(n<=1){
+            return "Not a Prime Number";
         }
+        
         if(n>1){
             for(int i=2;i<n;i++){
                 if(n%i==0){
