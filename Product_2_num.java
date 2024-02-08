60555d08a4ff01a64f640b1d9046ff365304019a
Om Jadav <omjadav04@gmail.com>
2024-02-08 16:46:48 +0530

    product of two numbers

---
+++ Product_2_num.java
@@ -0,0 +1,10 @@
+public class Product_2_num {
+    public static void main(String[] args) {
+        int answer=Product(5,4);
+        System.out.println("answer:"+answer);
+    }
+    public static int Product(int a,int b) {
+        int answer=a*b;
+        return answer;
+    }
+}
