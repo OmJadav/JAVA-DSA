3ae4e5323974f92c0e3bf634683ccc1fe96a3ff3
Om Jadav <omjadav04@gmail.com>
2024-02-10 22:59:00 +0530

    function practice questions

--- Functions_Questions.java
+++ Functions_Questions.java
@@ -13,12 +13,12 @@ public class Functions_Questions {
         System.out.println("Sum of Digit of Number "+num+" = "+sumOfDigit(num));
     }
 
-    // Write a Java method to compute the average of three numbers..
+    //Que 1 Write a Java method to compute the average of three numbers..
     public static double aveOfthree(double a,double b,double c){
             return (a+b+c)/3;
     }
 
-    // Write a method named isEven that accepts an int argument. The method
+    //Que 2 Write a method named isEven that accepts an int argument. The method
     // should return true if the argument is even, or false otherwise. Also write a program to test your
     // method.
     public static Boolean isEven(int n){
@@ -28,7 +28,7 @@ public class Functions_Questions {
         return false;
     }
 
-    // Write a Java program to check if a number is a palindrome in Java? ( 121 is a
+    //Que 3 Write a Java program to check if a number is a palindrome in Java? ( 121 is a
     // palindrome, 321 is not) 
     // A number is called a palindrome if the number is equal to the reverse of a number e.g., 121 is a
     // palindrome because the reverse of 121 is 121 itself. On the other hand, 321 is not a
@@ -49,7 +49,7 @@ public class Functions_Questions {
     }
 
 
-    // Write a Java method to compute the sum of the digits in an integer
+    //Que 4 Write a Java method to compute the sum of the digits in an integer
 
     public static int sumOfDigit(int num){
         int sumOfdigits=0;
