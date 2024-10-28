7e1573a1728864195646988a9ffcb1e778bb75e5
Om Jadav <omjadav04@gmail.com>
2024-10-28 14:17:48 +0530

    update

---
+++ BinarySearch.java
@@ -0,0 +1,33 @@
+//704. https://leetcode.com/problems/binary-search
+
+//for sorted Array
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] nums = { 4, 3, 2, 1 };
+        int target = 2;
+        System.out.println(Search_key(nums, target));
+    }
+
+    public static int Search_key(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                System.out.println(" Target found at Index " + mid + " :" + nums[mid]);
+                return mid;
+            }
+            // if (nums[mid] < target) { //for ascending order shorted array
+            // start = mid + 1;
+            // } else {
+            // end = mid - 1;
+            // }
+            if (nums[mid] > target) { // for descending order shorted array
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ Sort_Array.java
@@ -0,0 +1,16 @@
+public class Sort_Array {
+    public static void main(String args[]) {
+        int[] arr = { 1, 4, -3, 3 };
+        System.out.println(Sort(arr));
+    }
+
+    public static int[] Sort(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int element : arr) {
+            for (int j = 0; j < arr.length; j++) {
+
+            }
+        }
+        return new int[] {};
+    }
+}
